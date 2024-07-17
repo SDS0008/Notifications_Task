ebe9abe7829f6ae4698ff98c220319dd20589a04
SDS0008 <sdivyasumanth008@gmail.com>
2024-07-17 20:07:20 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
+
 import './App.css';
 
-function App() {
+
+
+
+let dataobj = [
+  {
+    imgUrl: "https://static.vecteezy.com/system/resources/previews/010/152/436/original/tick-check-mark-icon-sign-symbol-design-free-png.png",
+    msg: "informatiom message",
+  },
+  {
+    imgUrl: "https://static.vecteezy.com/system/resources/previews/010/152/436/original/tick-check-mark-icon-sign-symbol-design-free-png.png",
+    msg: "sucess message",
+  },
+  {
+    imgUrl: "https://tse3.mm.bing.net/th?id=OIP.CdIXApf1wUJoFGWZmZ78ZAHaHa&pid=Api&P=0&h=220",
+    msg: "Warning message",
+  },
+  {
+    imgUrl: "https://tse1.mm.bing.net/th?id=OIP.xR28qga2d1i-vMrMpuAujQHaHK&pid=Api&P=0&h=220",
+    msg: "Danger message",
+  }
+]
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <section className='container'>
+      <h1 className="msg-heading">
+        Notifications
+      </h1>
+
+      {
+        dataobj.map((eachobj) => {
+          return (
+            < Notification
+              imgUrl={ eachobj.imgUrl }
+              msg={ eachobj.msg }
+            />
+          )
+
+
+        })
+      }
+    </section>
+  );
+}
+
+
+const Notification = (props) => {
+
+  const styles = {
+    img: {
+      height: "20px",
+      width: "20px"
+    }
+  }
+  return (
+    <div className="msg-bg">
+
+      <div className='Div-div'>
+        <img src={ props.imgUrl } alt="" style={ styles.img } />
+
+        <p>{ props.msg }</p>
+      </div>
+
     </div>
   );
 }
+
+
+
 
 export default App;
